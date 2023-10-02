ec3e51e5131542a195889628d109fbae307d1d87
kshitijbahul <chunks.3@gmail.com>
2023-10-02 22:04:14 +0200

    Added another print message

--- utils/index.js
+++ utils/index.js
@@ -2,5 +2,10 @@ const printMessgae = ({ msg }) => {
     console.log(`Printing the message :: ${msg}`);
 }
 
+const printMessageAndGreeting  = ({ msg, greeting }) => {
+    console.log(`Printing the greeting :: ${greeting}`);
+    console.log(`Printing the message :: ${msg}`);
+}   
+
 
 module.exports = { printMessgae }
